5fbc25c89597e22d91a0954cadd66a0c9dd54358
zouxiaoliang <295919168@qq.com>
2020-01-07 16:07:22 +0800

    修改低级错误

--- Formula/zipper.rb
+++ Formula/zipper.rb
@@ -2,7 +2,7 @@ class Zipper < Formula
   desc "C++ wrapper around minizip compression library"
   homepage "https://github.com/sebastiandev/zipper"
   url "https://github.com/sebastiandev/zipper.git", :tag => "v1.0.1"
-  haad "https://github.com/sebastiandev/zipper.git"
+  head "https://github.com/sebastiandev/zipper.git"
   version "v1.0.1"
 
   depends_on "cmake" => :build
