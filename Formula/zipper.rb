7f88f63b184ea9aaa0142b7f0eee70b90f6dd342
zouxiaoliang <295919168@qq.com>
2020-01-07 15:46:01 +0800

    增加
    增加zipper库的支持

---
+++ Formula/zipper.rb
@@ -0,0 +1,17 @@
+class Zipper < Formula
+  desc "C++ wrapper around minizip compression library"
+  homepage "https://github.com/sebastiandev/zipper"
+  url "https://github.com/sebastiandev/zipper.git", :tag => "v1.0.1"
+  version "v1.0.1"
+
+  depends_on "cmake" => :build
+
+  def install
+    system "cmake", ".", *std_cmake_args
+    system "make", "install"
+  end
+
+  test do
+    system "false"
+  end
+end
